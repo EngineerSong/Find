2ef37c18168707de82358e8d01f1edc4d3b32a25
barara <barara@bararadeiMac.AirDream>
2015-07-23 14:36:30 +0800

    Initial Commit

---
+++ Find/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Find
+//
+//  Created by barara on 15/7/23.
+//  Copyright (c) 2015年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
